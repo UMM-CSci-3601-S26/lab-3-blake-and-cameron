3249b61f64ef8cc2cbc7bc8a926fa8302ec89de5
Cameron <walk1524@umn.edu>
2026-02-19 14:20:22 -0600

    Added componets to the todo folder.
    Co-authored-by: Blake L Thompson <thom9170@users.noreply.github.com>

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -12,8 +12,11 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
+import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
+import { Todo, todoCategory } from './todo';
+import { TodoCardComponent } from './todo-card.component';
 import { TodoService } from './todo.service';
-
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 /**
  * A component that displays a list of users, either as a grid
  * of cards or as a vertical list.
@@ -45,38 +48,50 @@ import { TodoService } from './todo.service';
   ],
 })
 export class TodoListComponent {
-  // userService the `UserService` used to get users from the server
+  // TodoService the `TodoService` used to get todos from the server
   private TodoService = inject(TodoService);
   // snackBar the `MatSnackBar` used to display feedback
   private snackBar = inject(MatSnackBar);
 
+  todo_id = signal<string | undefined>(undefined);
   todoOwner = signal<string | undefined>(undefined);
   todoStatus = signal<boolean | undefined>(undefined);
   todoBody = signal<string | undefined>(undefined);
   todoCategory = signal<string | undefined>(undefined);
 
-  // viewType = signal<'card' | 'list'>('card');
+  viewType = signal<'card' | 'list'>('card');
 
   errMsg = signal<string | undefined>(undefined);
 
-  // The `Observable`s used in the definition of `serverFilteredUsers` below need
-  // observables to react to, i.e., they need to know what kinds of changes to respond to.
-  // We want to do the age and role filtering on the server side, so if either of those
-  // text fields change we want to re-run the filtering. That means we have to convert both
-  // of those _signals_ to _observables_ using `toObservable()`. Those are then used in the
-  // definition of `serverFilteredUsers` below to trigger updates to the `Observable` there.
-  //private TodoRole$ = toObservable(this.userRole);
-  //private TodoAge$ = toObservable(this.userAge);
+  private todoOwner$ = toObservable(this.todoOwner);
+  private todoStatus$ = toObservable(this.todoStatus);
 
   // We ultimately `toSignal` this to be able to access it synchronously, but we do all the RXJS operations
   // "inside" the `toSignal()` call processing and transforming the observables there.
-  // serverFilteredUsers =
-  //    // This `combineLatest` call takes the most recent values from these two observables (both built from
-  // signals as described above) and passes them into the following `.pipe()` call. If either of the
-  // `userRole` or `userAge` signals change (because their text fields get updated), then that will trigger
-  //     // the corresponding `userRole$` and/or `userAge$` observables to change, which will cause `combineLatest()`
-  // to send a new pair down the pipe.
-  // toSignal(
+  serverFilteredTodos =
+    toSignal(
+      combineLatest([this.todoOwner$,]).pipe(
+        switchMap(([owner]) =>
+          this.TodoService.getTodos({
+            owner,
+          })
+        )));
+
+        // `catchError` is used to handle errors that might occur in the pipeline. In this case `userService.getUsers()`
+        // can return errors if, for example, the server is down or returns an error. This catches those errors, and
+        // sets the `errMsg` signal, which allows error messages to be displayed.
+        catchError((err) => {
+          if (!(err.error instanceof ErrorEvent)) {
+            this.errMsg.set(
+              `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
+            );
+          }
+
+          this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
+          // `catchError` needs to return the same type. `of` makes an observable of the same type, and makes the array still empty
+          return of<Todo[]>([]);
+        }):
+
   // No need for fancy RXJS stuff. We do the fancy RXJS stuff where we call `toSignal`, i.e., up in
   // the definition of `serverFilteredUsers` above.
   // `computed()` takes the value of one or more signals (`serverFilteredUsers` in this case) and
@@ -89,7 +104,6 @@ export class TodoListComponent {
     const serverFilteredTodos = this.filteredTodos();
     return this.TodoService.filterTodo(serverFilteredTodos, {
       owner: this.todoOwner(),
-      category: this.todoCategory(),
     });
   });
 }
--- client/src/app/todos/todo.ts
+++ client/src/app/todos/todo.ts
@@ -5,3 +5,5 @@ export interface Todo {
   body: string;
   category: string;
 }
+
+export type todoCategory = 'video games' | 'software design' | 'groceries' | 'homework';
