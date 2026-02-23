6315169e6ec52932e94a25cee8afc2674b2fbd09
Blake Thompson <Thom9170@morris.umn.edu>
2026-02-23 03:13:32 -0600

    Added backend logic and fixed html,ts files

--- client/src/app/todos/todo-card.component.ts
+++ client/src/app/todos/todo-card.component.ts
@@ -4,12 +4,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink } from '@angular/router';
-import { Todo } from './todos';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-todo-card',
-  templateUrl: './todo-card.component.html',
-  styleUrls: ['./todo-card.component.scss'],
+  templateUrl: './todo-card.components.html',
+  styleUrls: ['./todo-card.components.scss'],
   imports: [MatCardModule, MatButtonModule, MatListModule, MatIconModule, RouterLink]
 })
 export class TodoCardComponent {
--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -13,25 +13,18 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink } from '@angular/router';
 import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
+
 import { Todo, todoCategory } from './todo';
 import { TodoCardComponent } from './todo-card.component';
 import { TodoService } from './todo.service';
+
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-/**
- * A component that displays a list of users, either as a grid
- * of cards or as a vertical list.
- *
- * The component supports local filtering by name and/or company,
- * and remote filtering (i.e., filtering by the server) by
- * role and/or age. These choices are fairly arbitrary here,
- * but in "real" projects you want to think about where it
- * makes the most sense to do the filtering.
- */
+
 @Component({
   selector: 'app-todo-list-component',
+  standalone: true,
   templateUrl: 'todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
-  providers: [],
   imports: [
     MatCardModule,
     MatFormFieldModule,
@@ -40,6 +33,7 @@ import { toObservable, toSignal } from '@angular/core/rxjs-interop';
     MatSelectModule,
     MatOptionModule,
     MatRadioModule,
+    TodoCardComponent,
     MatListModule,
     RouterLink,
     MatButtonModule,
@@ -48,62 +42,57 @@ import { toObservable, toSignal } from '@angular/core/rxjs-interop';
   ],
 })
 export class TodoListComponent {
-  // TodoService the `TodoService` used to get todos from the server
-  private TodoService = inject(TodoService);
-  // snackBar the `MatSnackBar` used to display feedback
+
+
+  private todoService = inject(TodoService);
   private snackBar = inject(MatSnackBar);
 
-  todo_id = signal<string | undefined>(undefined);
   todoOwner = signal<string | undefined>(undefined);
+  todoCategory = signal<todoCategory | undefined>(undefined);
   todoStatus = signal<boolean | undefined>(undefined);
   todoBody = signal<string | undefined>(undefined);
-  todoCategory = signal<string | undefined>(undefined);
 
   viewType = signal<'card' | 'list'>('card');
 
   errMsg = signal<string | undefined>(undefined);
 
-  private todoOwner$ = toObservable(this.todoOwner);
+  private todoCategory$ = toObservable(this.todoCategory);
   private todoStatus$ = toObservable(this.todoStatus);
 
-  // We ultimately `toSignal` this to be able to access it synchronously, but we do all the RXJS operations
-  // "inside" the `toSignal()` call processing and transforming the observables there.
-  serverFilteredTodos =
-    toSignal(
-      combineLatest([this.todoOwner$,]).pipe(
-        switchMap(([owner]) =>
-          this.TodoService.getTodos({
-            owner,
-          })
-        )));
+  serverFilteredTodos = toSignal(
+    combineLatest([this.todoCategory$, this.todoStatus$]).pipe(
+      switchMap(([category, status]) =>
+        this.todoService.getTodos({
+          category,
+          status,
+        })
+      ),
 
-        // `catchError` is used to handle errors that might occur in the pipeline. In this case `userService.getUsers()`
-        // can return errors if, for example, the server is down or returns an error. This catches those errors, and
-        // sets the `errMsg` signal, which allows error messages to be displayed.
-        catchError((err) => {
-          if (!(err.error instanceof ErrorEvent)) {
-            this.errMsg.set(
-              `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
-            );
-          }
+      catchError((err) => {
+        if (!(err.error instanceof ErrorEvent)) {
+          this.errMsg.set(
+            `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
+          );
+        }
 
-          this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
-          // `catchError` needs to return the same type. `of` makes an observable of the same type, and makes the array still empty
-          return of<Todo[]>([]);
-        }):
+        this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
 
-  // No need for fancy RXJS stuff. We do the fancy RXJS stuff where we call `toSignal`, i.e., up in
-  // the definition of `serverFilteredUsers` above.
-  // `computed()` takes the value of one or more signals (`serverFilteredUsers` in this case) and
-  // _computes_ the value of a new signal (`filteredUsers`). Angular recognizes when any signals
-  // in the function passed to `computed()` change, and will then call that function to generate
-  // the new value of the computed signal.
-  // In this case, whenever `serverFilteredUsers` changes (e.g., because we change `userName`), then `filteredUsers`
-  // will be updated by rerunning the function we're passing to `computed()`.
+        return of<Todo[]>([]);
+      }),
+
+      tap(() => {
+
+      })
+    )
+  );
+
+
   filteredTodos = computed(() => {
-    const serverFilteredTodos = this.filteredTodos();
-    return this.TodoService.filterTodo(serverFilteredTodos, {
+    const serverFilteredTodos = this.serverFilteredTodos();
+
+    return this.todoService.filterTodo(serverFilteredTodos, {
       owner: this.todoOwner(),
+      category: this.todoCategory(),
     });
   });
 }
